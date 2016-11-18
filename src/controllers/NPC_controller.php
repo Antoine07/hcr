7e704b24212a67cb9c369e079ba429e29a43a189
“Pierre-SAIGOT” <psaigot@gmail.com>
2016-11-18 17:02:38 +0100

    modif du router, NPC CONTROLLER

--- src/controllers/NPC_controller.php
+++ src/controllers/NPC_controller.php
@@ -8,11 +8,3 @@ function generate_NPCs_action($nb_pilotes, $nb_mecaniciens){
 	$npc_manager->store($pilotes);
 	$npc_manager->store($mecaniciens);
 }
-
-function show_NPCs_action(){
-
-	$npc_manager = new game\NPC_manager(get_pdo());
-	$NPCs = $npc_manager->get_all();
-
-	include('../views/npc_list.php');
-}
