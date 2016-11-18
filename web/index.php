b2fab08b8ae40d35209f826d07d9f041068239c9
Arendil <alvescanteiro.joel@gmail.com>
2016-11-18 15:30:29 +0100

    modif index

--- web/index.php
+++ web/index.php
@@ -11,10 +11,13 @@ require_once __DIR__.'/../app.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ( '/' === $uri) {
-	$modul = new game\Module;
+	$equipment_manager = new game\Equipment_manager(get_pdo());
+	$modul_list = $equipment_manager->generate(4); 
 	echo '<pre>';
-	print_r($modul);
+	print_r($modul_list);
 	echo '</pre>';
+	$equipment_manager->store($modul_list);
+
 }
 else{
 	header('HTTP/1.1 404 Not Found');
