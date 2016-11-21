7949ce537e1effb126220cd61d28013856399cbd
“Pierre-SAIGOT” <psaigot@gmail.com>
2016-11-21 10:43:15 +0100

    correction conflit router

--- web/index.php
+++ web/index.php
@@ -7,21 +7,6 @@ require_once __DIR__.'/../app.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-<<<<<<< HEAD
-if ( '/' === $uri) {
-	home_action();
-}elseif ( '/index.php/login' === $uri) {
-	login_action();
-}elseif ( '/index.php/bar' === $uri) {
-	bar_action();
-}elseif ( '/index.php/qg' === $uri) {
-	qg_action();
-}elseif ( '/index.php/shop' === $uri) {
-	shop_action();
-}elseif ( '/index.php/race' === $uri) {
-	race_action();
-} else{
-=======
 $prefix = '/' . getEnv('URL_PREFIX');
 
 if ( '/' === $uri) {
@@ -38,7 +23,6 @@ if ( '/' === $uri) {
 	race_action();
 }
 else{
->>>>>>> feature_routing
 	header('HTTP/1.1 404 Not Found');
 	echo '<html><body><h1>Page Not Found</h1></body></html>';
 }
