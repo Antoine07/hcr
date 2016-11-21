1ea8961a87534986acb0275a2626a439f942c9c4
jgast <gastineau.julien@hotmail.fr>
2016-11-21 17:00:41 +0100

    Fonction h pour htmlentities

--- src/utils.php
+++ src/utils.php
@@ -35,29 +35,10 @@ function f_rand($min=0,$max=1,$mul=10000){
     return mt_rand($min*$mul,$max*$mul)/$mul;
 }
 
-class Security
-    {
-        // Données entrantes
-        public static function bdd($string)
-        {
-            // On regarde si le type de string est un nombre entier (int)
-            if(ctype_digit($string))
-            {
-                $string = intval($string);
-            }
-            // Pour tous les autres types
-            else
-            {
-                $string = mysql_real_escape_string($string);
-                $string = addcslashes($string, '%_');
-            }
-                
-            return $string;
-        }
+// Données sortantes
+function h($str, $option = false)
+{
+	if($option) return $str;
 
-        // Données sortantes
-        public static function h($string)
-        {
-            return htmlentities($string);
-        }
-    }
+    return htmlentities($str);
+}
