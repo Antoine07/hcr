a23fc41cec9bed3a29e731285bc32d2fea807bd9
Bastien Lhuaire <bastien.lhuaire@outlook.fr>
2016-11-22 15:12:13 +0100

    ajout des activités par défaut dans seeders.sql

---
+++ databases/seeders.sql
@@ -0,0 +1,5 @@
+INSERT INTO `activities` (`name`,`strength`) VALUES ('Utilise : La Force pour les nuls', 5);
+INSERT INTO `activities` (`name`,`stamina`) VALUES ("Utilise : L'Endurance pour les nuls", 5);
+INSERT INTO `activities` (`name`,`intelligence`) VALUES ("Utilise : L'Intelligence pour les nuls", 5);
+INSERT INTO `activities` (`name`,`dexterity`) VALUES ('Utilise : La Dextérité pour les nuls', 5);
+INSERT INTO `activities` (`name`,`speed`) VALUES ('Utilise : La Vitesse pour les nuls', 5);
