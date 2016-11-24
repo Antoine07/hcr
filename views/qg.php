230c4bbfda01c66da42f7bd9558fbb036e140df5
Faktori <clement.dussol@gmail.com>
2016-11-24 08:25:39 +0100

    affichage portraits npcs dans le qg

--- views/qg.php
+++ views/qg.php
@@ -3,19 +3,21 @@
 		<div class="row">
 			<div class="col s6">
 				<div class="qg_card">
-					<h6><?php echo $pilot->get_name();?></h6>
-						<ul class="qg_stats">
-							<li>Intelligence: <?php  echo $pilot->get_stats('intelligence')?></li>
-							<span>|</span>
-							<li>Dextérité: <?php  echo $pilot->get_stats('dexterity')?></li>
-							<span>|</span>
-							<li>Endurance: <?php  echo $pilot->get_stats('stamina')?></li>
-							<span>|</span>
-							<li>Rapidité: <?php  echo $pilot->get_stats('speed')?></li>
-							<span>|</span>
-							<li>Force: <?php  echo $pilot->get_stats('strength')?></li>
-						</ul>
-
+					<div class="flex">
+						<img class="bar_portrait" src="../images/<?php echo $pilot->get_id() ?>.png" alt="">
+						<h6><?php echo $pilot->get_name();?></h6>	
+					</div>
+					<ul class="qg_stats">
+						<li>Intelligence: <?php  echo $pilot->get_stats('intelligence')?></li>
+						<span>|</span>
+						<li>Dextérité: <?php  echo $pilot->get_stats('dexterity')?></li>
+						<span>|</span>
+						<li>Endurance: <?php  echo $pilot->get_stats('stamina')?></li>
+						<span>|</span>
+						<li>Rapidité: <?php  echo $pilot->get_stats('speed')?></li>
+						<span>|</span>
+						<li>Force: <?php  echo $pilot->get_stats('strength')?></li>
+					</ul>
 					<form action="<?php echo url('qg') ?>" method="POST">
 						<div class="input-field"   title="Activité assginé avant la course. Elle permet d'augmenter les compétences de ce personnage">
 							<select name="activity1">
@@ -40,7 +42,10 @@
 		<div class="row">
 			<div class="col s6">
 				<div class="qg_card">
-					<h6><?php echo $mechanic->get_name();?></h6>
+					<div class="flex">
+						<img class="bar_portrait" src="../images/<?php echo $mechanic->get_id() ?>.png" alt="">
+						<h6><?php echo $mechanic->get_name();?></h6>
+					</div>
 						<ul class="qg_stats">
 							<li>Intelligence: <?php  echo $mechanic->get_stats('intelligence')?></li>
 							<span>|</span>
